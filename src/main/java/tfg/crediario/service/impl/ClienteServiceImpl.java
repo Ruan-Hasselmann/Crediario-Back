59c410a0c2b25a33048c69e669c8e203d1f63177
Ruan <ruanhasselmanndasilva@gmail.com>
2023-08-12 15:22:15 -0300

    Adequando service cliente

---
+++ src/main/java/tfg/crediario/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,48 @@
+package tfg.crediario.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tfg.crediario.entity.Cliente;
+import tfg.crediario.repository.ClienteRepository;
+import tfg.crediario.service.ClienteService;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAllByStatus();
+    }
+
+    public Optional<Cliente> getClienteById(Integer id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente createCliente(Cliente cliente) {
+        cliente.setStatus(true);
+        cliente.setDataCadastro(getDateTime());
+        return clienteRepository.save(cliente);
+    }
+
+    public Integer updateCliente(Integer id, Cliente cliente) {
+        return clienteRepository.updateCliente(cliente.getNome(), cliente.getCpf(), cliente.getRg(), cliente.getTelefone(), id);
+    }
+
+    public Integer updateStatusCliente(Integer id, Boolean status) {
+        return clienteRepository.updateStatus(status, id);
+    }
+
+    private String getDateTime() {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+}
